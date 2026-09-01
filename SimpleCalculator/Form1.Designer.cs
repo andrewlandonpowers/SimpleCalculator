fe75c58b228bba0b82087542bdb085c87a928e3c
Andrew Powers <andrewlandonpowers@gmail.com>
2026-09-01 10:03:07 -0400

    Finished assignment

--- SimpleCalculator/Form1.Designer.cs
+++ SimpleCalculator/Form1.Designer.cs
@@ -36,6 +36,8 @@
             textBox2 = new TextBox();
             textBox3 = new TextBox();
             textBox4 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
             SuspendLayout();
             // 
             // label1
@@ -43,36 +45,37 @@
             label1.AutoSize = true;
             label1.Location = new Point(70, 52);
             label1.Name = "label1";
-            label1.Size = new Size(59, 25);
+            label1.Size = new Size(101, 25);
             label1.TabIndex = 0;
-            label1.Text = "label1";
+            label1.Text = "Operand 1:";
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.Location = new Point(70, 106);
             label2.Name = "label2";
-            label2.Size = new Size(59, 25);
+            label2.Size = new Size(88, 25);
             label2.TabIndex = 1;
-            label2.Text = "label2";
+            label2.Text = "Operator:";
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.Location = new Point(70, 155);
             label3.Name = "label3";
-            label3.Size = new Size(59, 25);
+            label3.Size = new Size(101, 25);
             label3.TabIndex = 2;
-            label3.Text = "label3";
+            label3.Text = "Operand 2:";
             // 
             // label4
             // 
             label4.AutoSize = true;
             label4.Location = new Point(70, 205);
             label4.Name = "label4";
-            label4.Size = new Size(59, 25);
+            label4.Size = new Size(63, 25);
             label4.TabIndex = 3;
-            label4.Text = "label4";
+            label4.Text = "Result:";
+            label4.Click += label4_Click;
             // 
             // textBox1
             // 
@@ -80,6 +83,7 @@
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(150, 31);
             textBox1.TabIndex = 4;
+            textBox1.TextChanged += textBox1_TextChanged;
             // 
             // textBox2
             // 
@@ -87,6 +91,7 @@
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(150, 31);
             textBox2.TabIndex = 5;
+            textBox2.TextChanged += textBox2_TextChanged;
             // 
             // textBox3
             // 
@@ -94,19 +99,45 @@
             textBox3.Name = "textBox3";
             textBox3.Size = new Size(150, 31);
             textBox3.TabIndex = 6;
+            textBox3.TextChanged += textBox3_TextChanged;
             // 
             // textBox4
             // 
             textBox4.Location = new Point(175, 205);
             textBox4.Name = "textBox4";
+            textBox4.ReadOnly = true;
             textBox4.Size = new Size(150, 31);
             textBox4.TabIndex = 7;
             // 
+            // button1
+            // 
+            button1.Location = new Point(59, 261);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 8;
+            button1.Text = "&Calculate";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(196, 261);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 9;
+            button2.Text = "E&xit";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
             // Form1
             // 
+            AcceptButton = button1;
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
+            CancelButton = button2;
             ClientSize = new Size(385, 332);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Controls.Add(textBox4);
             Controls.Add(textBox3);
             Controls.Add(textBox2);
@@ -131,5 +162,7 @@
         private TextBox textBox2;
         private TextBox textBox3;
         private TextBox textBox4;
+        private Button button1;
+        private Button button2;
     }
 }
--- SimpleCalculator/Form1.cs
+++ SimpleCalculator/Form1.cs
@@ -6,5 +6,94 @@ namespace SimpleCalculator
         {
             InitializeComponent();
         }
+
+        decimal operand1 = 0.0m;
+        string operator1;
+        decimal operand2 = 0.0m;
+
+        decimal result = 0.0m;
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            clearResult();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            clearResult();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            clearResult();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (decimal.TryParse(textBox3.Text, out decimal parsedOperand))
+            {
+                operand2 = parsedOperand;
+            }
+            else
+            {
+                operand2 = 0.0m;
+            }
+
+            if (decimal.TryParse(textBox1.Text, out decimal parsedOperand2))
+            {
+                operand1 = parsedOperand2;
+            }
+            else
+            {
+                operand1= 0.0m;
+            }
+
+            result = Calculate(operand1, operator1, operand2);
+
+            result = Math.Round(result, 4);
+
+            textBox4.Text = Convert.ToString(result);
+
+            textBox1.Focus();
+        }
+
+
+        private decimal Calculate(decimal operand1, string operator1, decimal operand2)
+        {
+            if (textBox2.Text == "/")
+            {
+                result = (Math.Round((operand1 / operand2), 4));
+            }
+            else if (textBox2.Text == "+")
+            {
+                result = (Math.Round((operand1 + operand2), 4));
+            }
+            else if (textBox2.Text == "-")
+            {
+                result = (Math.Round((operand1 - operand2), 4));
+            }
+            else if (textBox2.Text == "*")
+            {
+                result = (Math.Round((operand1 * operand2), 4));
+            }
+            else { result = 0.0m; }
+
+            return result;
+        }
+
+        private void clearResult()
+        {
+            textBox4.Clear();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
